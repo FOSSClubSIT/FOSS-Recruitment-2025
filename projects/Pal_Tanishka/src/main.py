32ada2361be77097b013ecf83ee695bcacf6dd4f
Tanishka Pal <tanishka.ppal@gmail.com>
2025-08-25 20:19:49 +0530

    Loaded the dataset

--- projects/Pal_Tanishka/src/main.py
+++ projects/Pal_Tanishka/src/main.py
@@ -15,3 +15,12 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler, PolynomialFeatur
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix, classification_report
+
+# =============================
+# 2. Load Dataset
+# =============================
+df = pd.read_csv(r"C:\Users\tanis\Downloads\archive\crop_yield.csv")  # CSV included in repo
+print("Dataset Preview:")
+print(df.head())
+print("\nDataset Info:")
+print(df.info())
