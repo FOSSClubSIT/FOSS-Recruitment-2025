412188c0b1845941eb9da476d8b2fee55f859d8d
Adwaiy <adwaiy1510@gmail.com>
2025-08-26 19:28:58 +0530

    Built the first part of the code. The screen is set up, along with the playable character being able to move and restrained within the limits of the window. Might upscale the game a bit.

---
+++ projects/Gupta_Adwaiy/pythonmainfinal.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+import time
+
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("A Ride to Hell")
+
+BG = pygame.transform.scale(pygame.image.load("hell.jpg"), (WIDTH, HEIGHT))
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+PLAYER_VEL = 5
+
+def draw(player):
+    WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "violet", player)
+
+    pygame.display.update()
+
+def main():
+    run = True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_d] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
+            player.x += PLAYER_VEL
+           
+        draw(player)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
